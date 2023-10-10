e6b3691b47b5ccdb128ebd097c27e52fd4358c11
nodarchik <chikadzee@gmail.com>
2023-10-10 16:56:22 +0400

    Registration works

--- storage-api/src/config/db.ts
+++ storage-api/src/config/db.ts
@@ -3,5 +3,5 @@ import { MongoClient } from 'mongodb';
 export const connectDB = async () => {
     const client = new MongoClient(process.env.MONGO_URI!);
     await client.connect();
-    return client.db('MongoClient');
+    return client.db('andersen');
 };
--- storage-api/src/routes/userRoutes.ts
+++ storage-api/src/routes/userRoutes.ts
@@ -1,55 +1,39 @@
 import { Router } from 'express';
-import { connectDB } from '../config/db';
-import { User } from '../models/User';
-import { ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import jwt from 'jsonwebtoken';
 
 const router = Router();
 
+// Define the /register route
 router.post('/register', async (req, res) => {
-    const db = await connectDB();
-    const user: User = req.body;
+  const client = new MongoClient(process.env.MONGO_URI!);
+  try {
+    await client.connect();
+    const db = client.db();
+    const user = req.body;
     // We should hash the password before storing it, but skipping for simplicity.
-    await db.collection('users').insertOne(user);
+
+    const insertResult = await db.collection('users').insertOne(user);
+
+    if (!insertResult.acknowledged) {
+      return res.status(500).send('Failed to register user');
+    }
+
+    const newUserId = insertResult.insertedId;
 
     // Generate JWT Token
-    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.JWT_SECRET!, {
-        expiresIn: '1h',
+    const token = jwt.sign({ _id: newUserId, username: user.username }, process.env.JWT_SECRET!, {
+      expiresIn: '1h',
     });
 
     // Send token in HTTP header
-    res.header('Authorization', token).status(201).send('User registered with token');
-});
-
-
-router.get('/', async (req, res) => {
-    const db = await connectDB();
-    const users = await db.collection('users').find().toArray();
-    res.status(200).json(users);
-});
-
-router.get('/:id', async (req, res) => {
-    const db = await connectDB();
-    const user = await db.collection('users').findOne({ _id: new ObjectId(req.params.id) });
-    if (!user) return res.status(404).send('User not found');
-    res.status(200).json(user);
-});
-
-router.put('/:id', async (req, res) => {
-    const db = await connectDB();
-    const updatedUser = await db.collection('users').updateOne(
-        { _id: new ObjectId(req.params.id) },
-        { $set: req.body }
-    );
-    if (!updatedUser.matchedCount) return res.status(404).send('User not found');
-    res.status(200).send('User updated');
-});
-
-router.delete('/:id', async (req, res) => {
-    const db = await connectDB();
-    const deletedUser = await db.collection('users').deleteOne({ _id: new ObjectId(req.params.id) });
-    if (!deletedUser.deletedCount) return res.status(404).send('User not found');
-    res.status(200).send('User deleted');
+    res.header('Authorization', token).status(201).send({message: 'User registered successfully', token: token});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to register user');
+  } finally {
+    await client.close();
+  }
 });
 
 export default router;
