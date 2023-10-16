d4d572b52771776a11f066fd3faa113cef819dda
nodarchik <chikadzee@gmail.com>
2023-10-16 13:54:55 +0400

    Refactored authapi added readmy's

---
+++ auth-api/src/services/authService.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { config } from '../config/config';
+import { AuthServiceInterface } from '../interfaces/AuthServiceInterface';
+import { generateToken, verifyToken } from '../utils/jwtUtils';
+
+
+export class AuthService implements AuthServiceInterface {
+    static blacklistedTokens: Record<string, boolean> = {};
+
+    async login(email: string, password: string): Promise<string> {
+        try {
+            const { data } = await axios.post(`${config.STORAGE_API_URL}/users/login`, { email, password });
+            return generateToken(data._id);
+        } catch (error) {
+            throw new Error('Failed to login');
+        }
+    }
+
+    logout(token: string): void {
+        AuthService.blacklistedTokens[token] = true;
+    }
+
+    validateToken(token: string): boolean {
+        if (AuthService.blacklistedTokens[token]) {
+            return false;
+        }
+        return verifyToken(token);
+    }
+}
