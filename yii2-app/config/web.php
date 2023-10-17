f07edd5c30c50a3a671b5dceb395cb1eb0588114
nodarchik <chikadzee@gmail.com>
2023-10-17 16:47:53 +0400

    Utilized .env

--- yii2-app/config/web.php
+++ yii2-app/config/web.php
@@ -4,59 +4,63 @@ use app\clients\AuthApiClient;
 use app\clients\UserApiClient;
 use app\clients\NewsApiClient;
 use app\services\UserService;
+use Dotenv\Dotenv;
+
+// Load environment variables from the .env file
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 $params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => getenv('APP_ID'),
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'defaultRoute' => 'user/index',
+    'defaultRoute' => getenv('APP_DEFAULT_ROUTE'),
     'components' => [
         'authApiClient' => [
             'class' => AuthApiClient::class,
             'client' => function() {
                 return new GuzzleHttp\Client([
-                    'timeout'  => 2.0,
+                    'timeout'  => (float) getenv('AUTH_API_CLIENT_TIMEOUT'),
                 ]);
             },
-            'baseUrl' => 'http://localhost:3001/api',
+            'baseUrl' => getenv('AUTH_API_BASE_URL'),
         ],
         'userApiClient' => [
             'class' => UserApiClient::class,
             'client' => function() {
                 return new GuzzleHttp\Client([
-                    'timeout'  => 2.0,
+                    'timeout'  => (float) getenv('USER_API_CLIENT_TIMEOUT'),
                 ]);
             },
-            'baseUrl' => 'http://localhost:3002/api',
+            'baseUrl' => getenv('USER_API_BASE_URL'),
         ],
         'newsApiClient' => [
             'class' => NewsApiClient::class,
             'client' => function() {
                 return new GuzzleHttp\Client([
-                    'timeout'  => 2.0,
+                    'timeout'  => (float) getenv('NEWS_API_CLIENT_TIMEOUT'),
                 ]);
             },
-            'baseUrl' => 'http://localhost:3002/api',
+            'baseUrl' => getenv('NEWS_API_BASE_URL'),
         ],
         'request' => [
-            'cookieValidationKey' => 'your-cookie-validation-key',
+            'cookieValidationKey' => getenv('APP_COOKIE_VALIDATION_KEY'),
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
+            'identityClass' => getenv('USER_IDENTITY_CLASS'),
+            'enableAutoLogin' => filter_var(getenv('USER_ENABLE_AUTO_LOGIN'), FILTER_VALIDATE_BOOLEAN),
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => getenv('ERROR_HANDLER_ACTION'),
         ],
         'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'useFileTransport' => true,
+            'class' => getenv('MAILER_CLASS'),
+            'useFileTransport' => filter_var(getenv('MAILER_USE_FILE_TRANSPORT'), FILTER_VALIDATE_BOOLEAN),
         ],
         'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'traceLevel' => (int) getenv('LOG_TRACE_LEVEL'),
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
@@ -64,25 +68,24 @@ $config = [
                 ],
             ],
         ],
-        'db' => $db,
         'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'enablePrettyUrl' => filter_var(getenv('APP_ENABLE_PRETTY_URL'), FILTER_VALIDATE_BOOLEAN),
+            'showScriptName' => filter_var(getenv('APP_SHOW_SCRIPT_NAME'), FILTER_VALIDATE_BOOLEAN),
             'rules' => [],
         ],
     ],
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
+if (filter_var(getenv('APP_DEBUG'), FILTER_VALIDATE_BOOLEAN)) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
+        'class' => getenv('DEBUG_MODULE_CLASS'),
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
+        'class' => getenv('GII_MODULE_CLASS'),
     ];
 }
 
